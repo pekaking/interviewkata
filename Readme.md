be91921ff5d9a12c34172ff3bb0bf4f997433294
Pascal Schöni <pascal.schoeni@olmero.ch>
2017-06-21 09:08:54 +0200

    Added first exercise

--- Readme.md
+++ Readme.md
@@ -1,13 +1,18 @@
-# Prerequisites
+# Installation
+## Prerequisites
 
 * node / npm installed on your system
 * npm install -g gulp-cli
 
-# How to install
+## How to install
 
 1. npm install
-2. gulp install
 
-# How to run
+## How to run
 
 1. gulp serve
+2. open http://localhost:3000
+
+# What to do
+
+1. You find all the instructions in the [app.js](static/app.js) File, located in the static folder of this project.
--- static/app.js
+++ static/app.js
@@ -1,3 +1,19 @@
 console.log("here we go");
 
-// lets do something with /lorem.html
+
+/*
+ * Hi there and welcome to this little coding kata. Here is what you should do:
+ * 
+ * 1. There is a file on the webserver, named lorem.html. It contains a lot of (html) text. 
+ *    Write a javascript function, which fetches the contents of this file asynchronously 
+ *    from the server and add the contents into the <div class="container"> div of the website.
+ * 
+ * 2. There are lots of o's in the imported text. As the letter o is very important for this exercise, 
+ *    we should highlight it. Highlight all the o's with a renovero-blue background (see styles.css for 
+ *    more information about that color) and white font color, a 30% bigger font size and add some 
+ *    padding so that every o stands out.
+ * 
+ * 3. The letter 'r' is also very important. Highlight it in the same way, but use the renovero-yellow
+ *    color this time.
+ */
+
