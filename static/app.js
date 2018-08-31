9814252e695e37860fb7e54c8899d926e1a0a287
Philip Kaufmann <39113949+pkaufi@users.noreply.github.com>
2018-08-31 10:40:19 +0200

    clarify some instructions

--- static/app.js
+++ static/app.js
@@ -3,17 +3,18 @@ console.log("here we go");
 
 /*
  * Hi there and welcome to this little coding kata. Here is what you should do in javascript:
- * 
+ * NOTE: do the exercices step by step
+ *
  * 1. There is a file on the webserver, named lorem.html. It contains a lot of (html) text. 
  *    Write a javascript function, which fetches the contents of this file asynchronously 
- *    from the server and add the contents into the <div class="container"> div of the website.
+ *    from the server and replaces the contents of the <div class="container"> div of the website.
  * 
  * 2. There are lots of o's in the imported text. As the letter o is very important for this exercise, 
- *    we should highlight it. Highlight all the o's with my-blue background (see styles.css for 
+ *    we should highlight it. Highlight all the o's (upper- and lowercase) with my-blue background (see styles.css for 
  *    more information about that color) and white font color, a 30% bigger font size and add some 
  *    padding so that every o stands out.
  * 
- * 3. The letter 'r' is also very important. Highlight it in the same way, but use the my-orange
+ * 3. The letter 'r' is also very important. Highlight it in the same way (upper- and lowercase), but use the my-orange
  *    color this time.
  * 
  * 4. Instead of text with "meaning", the PO wants to have all the words which are placed in paragraph
