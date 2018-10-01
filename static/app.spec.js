25afe083ff67738dd948474c793dac2882e47166
Unknown <pekaking@gmail.com>
2018-10-01 12:55:55 +0200

    Added highlighting of the letter O in the text.

--- static/app.spec.js
+++ static/app.spec.js
@@ -3,4 +3,10 @@ describe("Interview kata", () => {
     it("should work", () => {
         expect(1).toEqual(1);
     });
+
+    it("should add wrap letter O with span", () => {
+        const mockHtmlText = '<div>Some text with 2 letters to highlight.</div>';
+        const highlightedText = highlightLetterO(mockHtmlText);
+        expect(highlightedText.match(/<\/span>/g).length).toBe(2);
+    })
 });
